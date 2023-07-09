b015bcce26a21d3234fa5e9f17eb8c1ed9fdf028
Sergey <barabashka089@gmail.com>
2023-07-09 14:05:55 +0300

    Нахождение элемента в массиве

---
+++ Example010/Program.cs
@@ -0,0 +1,28 @@
+﻿// int [] array = {15,26,18,84,18,60,70,68};
+// int n = array.Length;
+// int find = 18;
+// int index = 0;
+// while (index<n)
+// {
+//     if (array[index] == find)
+//     {
+//         Console.WriteLine(index);
+//         index++;
+//         break;
+//     }
+// }
+
+int [] array = {15,26,18,84,18,60,70,68};
+int n = array.Length;
+int find = 18;
+int index = 0;
+while (index<n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+    
+        break;
+    }
+    index++;
+}
