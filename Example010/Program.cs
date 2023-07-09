aaa0140052526c23b1ce886269396e051e5da240
Sergey <barabashka089@gmail.com>
2023-07-09 16:07:07 +0300

    Конец второй лекции

--- Example010/Program.cs
+++ Example010/Program.cs
@@ -12,17 +12,17 @@
 //     }
 // }
 
-int [] array = {15,26,18,84,18,60,70,68};
-int n = array.Length;
-int find = 18;
-int index = 0;
-while (index<n)
-{
-    if (array[index] == find)
-    {
-        Console.WriteLine(index);
+// int [] array = {15,26,18,84,18,60,70,68};
+// int n = array.Length;
+// int find = 18;
+// int index = 0;
+// while (index<n)
+// {
+//     if (array[index] == find)
+//     {
+//         Console.WriteLine(index);
     
-        break;
-    }
-    index++;
-}
+//         break;
+//     }
+//     index++;
+// }
